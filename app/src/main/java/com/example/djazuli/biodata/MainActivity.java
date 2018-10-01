d6b61c719fa0cba7a364efb8b708024a423fba79
jazuli <djazulimuhtar@gamil.com>
2018-10-01 17:06:24 +0700

    uplod tugas biodata pertama

---
+++ app/src/main/java/com/example/djazuli/biodata/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.djazuli.biodata;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String no_telp = "082221417262";
+    String email = "Djazulimuhtar@gmail.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Email(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","djazulimuhtar@gmail.com", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Judul Email");
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+    }
+
+    public void Alamat(View view) {
+        Uri uri = Uri.parse("https://maps.google.com/?q=-6.973583,110.406324");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
+    public void Telepon(View view) {
+        Intent intentIndirect = new Intent(Intent.ACTION_DIAL);
+        intentIndirect.setData(Uri.parse("tel:" +no_telp));
+        startActivity(intentIndirect);
+    }
+}
